7f4af0afdd6aeb0432840b112655f2d3fcfc9eb5
Rokas <rokasasmena@gmail.com>
2020-06-25 16:42:53 +0300

    Initial commit

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Auxiliary';
+import classes from './Person.css';
+
+class Person extends Component {
+    render() {
+        console.log('[Person.js] rendering...');
+        return (
+            <Aux>
+                <p key="i1" onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!
+                </p>,
+                <p key="i2">{this.props.children}</p>,
+                <input
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
+            </Aux>
+        );
+    }
+}
+
+export default Person;
